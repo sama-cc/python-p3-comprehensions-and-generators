cdccc0d2940b3f3e16135d9415b91f7aef41efaf
Sam A Carpenter-Camhi <sam.camhi@gmail.com>
2023-07-20 01:04:10 -0400

    Passed Tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_list = [n for n in num_list if n % 2 == 0]
+    return even_list
     pass
 
 def make_exclamation(sentence_list):
+    exclamed_list = [n + "!" for n in sentence_list]
+    return exclamed_list
     pass
